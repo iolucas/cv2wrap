72c3b70536842a20d5f01c3318cae034a09b3de9
Lucas <lucas.o@live.com>
2016-05-04 09:44:42 -0300

    done2

--- setup.py
+++ setup.py
@@ -1,13 +1,20 @@
+#from distutils.core import setup
+#setup(
+  #name = 'cv2wrap',
+  #packages = ['cv2wrap'], # this must be the same as the name above
+  #version = '1.0',
+  #description = 'Wrapper for python opencv 2.4.12 32bit',
+  #author = 'Lucas V. Oliveira',
+  #author_email = 'lucas.o@live.com',
+  #url = 'https://github.com/lucasolivier/cv2wrap', # use the URL to the github repo
+  #download_url = 'https://github.com/lucasolivier/cv2wrap/tarball/1.0', # I'll explain this in a second
+  #keywords = ['opencv', 'cv2', 'python'], # arbitrary keywords
+  #classifiers = []
+#)
+
+
 from distutils.core import setup
-setup(
-  name = 'cv2wrap',
-  packages = ['cv2wrap'], # this must be the same as the name above
-  version = '1.0',
-  description = 'Wrapper for python opencv 2.4.12 32bit',
-  author = 'Lucas V. Oliveira',
-  author_email = 'lucas.o@live.com',
-  url = 'https://github.com/lucasolivier/cv2wrap', # use the URL to the github repo
-  download_url = 'https://github.com/lucasolivier/cv2wrap/tarball/1.0', # I'll explain this in a second
-  keywords = ['opencv', 'cv2', 'python'], # arbitrary keywords
-  classifiers = []
-)
+setup(name='cv2',
+      version='1.0',
+      py_modules=['cv2'],
+      )
